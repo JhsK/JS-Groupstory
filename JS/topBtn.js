ac18865694acdd7544e0c54eb7b900b991acc590
JhsK <qwe6293@nate.com>
2021-02-14 13:51:29 +0900

    [Feat] js 스크롤 top 버튼 적용

---
+++ JS/topBtn.js
@@ -0,0 +1,17 @@
+const topBtn = document.querySelector(".topBtn__i");
+
+window.addEventListener("scroll", (event) => {
+  if (event.target.defaultView.scrollY === 0) {
+    topBtn.style.color = "#000";
+  } else {
+    topBtn.style.color = "#aea1ea";
+  }
+});
+
+topBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
