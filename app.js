b88c7e9db1bcb242132d482a03e485a75aabcc43
JhsK <qwe6293@nate.com>
2021-05-03 22:10:00 +0900

    [Feat] User table 테스트

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ const session = require("express-session");
 const dotenv = require("dotenv");
 const path = require("path");
 
+const { sequelize } = require("./models");
+
 dotenv.config();
 const indexRouter = require("./routes");
 const userRouter = require("./routes/user");
 
 const app = express();
 app.set("port", process.env.PORT || 3001);
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("데이터베이스 연결 성공");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(morgan("dev"));
 app.use("/", express.static(path.join(__dirname, "public")));
---
+++ models/user.js
@@ -0,0 +1,42 @@
+const Sequelize = require("sequelize");
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        name: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+        pw: {
+          type: Sequelize.STRING(30),
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING(100),
+          unique: true,
+          allowNull: false,
+        },
+        power: {
+          type: Sequelize.BOOLEAN,
+          allowNull: true,
+        },
+        dept: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        underscored: false,
+        modelName: "User",
+        tableName: "User",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+  static associate(db) {}
+};
